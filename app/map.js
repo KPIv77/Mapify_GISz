dd243a8f62479184532ca7086cf7cf0904fb648f
KPIv77 <kitisakpholor@gmail.com>
2026-02-21 16:12:01 +0700

    Add main map page

--- app/map.js
+++ app/map.js
@@ -1,5 +1,6 @@
 // Create the map and set default view (Leaflet).
 // Set Bangkok as default center(13.736717, 100.523186) with zoom level 6.
+
 const map = L.map("map").setView([13.736717, 100.523186], 6);
 
 // Add OpenStreetMap tiles with attribution.
