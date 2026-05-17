2a64d7d3f239e58616436f0892170caf3ee61ca5
KPIv77 <kitisakpholor@gmail.com>
2026-05-17 21:25:44 +0700

    add button menu

--- Gis/src/App.tsx
+++ Gis/src/App.tsx
@@ -1,27 +1,45 @@
-// App.jsx
 import { useState } from 'react'
+import Sidebar from './menu'
+import type { MenuItem } from './menu'
+import './App.css'
 import './App.css'
 
+
+
 function App() {
+      // Setup menu items for bottom Sidebar
+    const menuItems: MenuItem[] = [
+        { id: 1, label: 'Search',  group: 'top',    onClick: () => console.log('Search') },
+        { id: 2, label: 'File', group: 'middle', onClick: () => console.log('Filter') },
+        { id: 3, label: 'map1',   group: 'bottom', onClick: () => console.log('map1') },
+        { id: 4, label: 'map2',   group: 'bottom', onClick: () => console.log('map2') },
+        { id: 5, label: 'map3',   group: 'bottom', onClick: () => console.log('map3') },
+    ]  
+
   return (
-    // fragment ครอบ component หลัก
     <>
       <div className="layout">
-
-        {/* Header แถบบนสุด */}
-        <header className="header">Map GIS</header>
+        <header className="header"
+        >
+            Map GIS
+            <div className="header-tools">
+                <button>Measure</button>
+                <button>Pin location</button>
+                <button>Polygon</button>
+                <button>ClearAll</button>
+            </div>
+        </header>
 
         {/* Body: sidebar + ฝั่งขวา */}
         <div className="body">
+            {/* Sidebar เมนูซ้าย */}
+            <Sidebar menuItems={menuItems} />
 
-          {/* Sidebar เมนูซ้าย */}
-          <aside className="sidebar">Menu</aside>
-
-          {/* ฝั่งขวา: map + toolbar */}
-          <div className="right">
-            <div className="map-container">Map</div>
-            <div className="toolbar">Toolbar</div>
-          </div>
+            {/* ฝั่งขวา: map + toolbar */}
+            <div className="right">
+                <div className="map-container">Map</div>
+                <div className="toolbar">Toolbar</div>
+            </div>
 
         </div>
       </div>
